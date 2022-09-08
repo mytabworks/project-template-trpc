6861b941c77343a665c3a13e459d15d4dd64683d
jr <fernando.tabamo@whitecobalt.com>
2022-09-09 02:03:01 +0800

    server session

---
+++ src/server/utils/session.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { unstable_getServerSession } from "next-auth"
+import nextAuthOptions from "./nextAuthOptions"
+
+export const getServerSession = async (ctx: {
+    req: NextApiRequest;
+    res: NextApiResponse;
+}) => {
+    
+    return await unstable_getServerSession(ctx.req, ctx.res, nextAuthOptions)
+}
+
+export type Session = (ReturnType<typeof getServerSession> extends Promise<infer R> ? R : any)
