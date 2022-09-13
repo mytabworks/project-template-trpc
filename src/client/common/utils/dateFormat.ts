953af1f14bb156d523020d048b5cbd75b37e6ab8
jr <fernando.tabamo@whitecobalt.com>
2022-09-14 00:52:07 +0800

    add no timezone date/time format

--- src/client/common/utils/dateFormat.ts
+++ src/client/common/utils/dateFormat.ts
@@ -9,3 +9,32 @@ export const dateTimeFormat = (date: string | Date, localization: string = 'en-G
 export const timeFormat = (date: string | Date, localization: string = 'en-GB', ...args: any[]) => {
     return new Date(date).toLocaleTimeString(localization, ...args)
 }
+
+const leadingZero = (num: number) => num <= 9 ? `0${num}` : num
+
+export const dateTimeWithoutTimezone = (date: Date) => {
+    const day = leadingZero(date.getDate())
+    const month = leadingZero(date.getMonth() + 1)
+    const year = date.getFullYear()
+    const hours = leadingZero(date.getHours())
+    const minutes = leadingZero(date.getMinutes())
+    const seconds = leadingZero(date.getSeconds())
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+}
+
+export const dateWithoutTimezone = (date: Date) => {
+    const day = leadingZero(date.getDate())
+    const month = leadingZero(date.getMonth() + 1)
+    const year = date.getFullYear()
+
+    return `${year}-${month}-${day}`
+}
+
+export const timeWithoutTimezone = (date: Date) => {
+    const hours = leadingZero(date.getHours())
+    const minutes = leadingZero(date.getMinutes())
+    const seconds = leadingZero(date.getSeconds())
+
+    return `${hours}:${minutes}:${seconds}`
+}
