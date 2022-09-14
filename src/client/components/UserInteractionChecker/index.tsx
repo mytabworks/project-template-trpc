baf1261ef7d0c877f2ec0570fe762f37499649db
jr <fernando.tabamo@whitecobalt.com>
2022-09-14 09:59:44 +0800

    best way to determine interaction

--- src/client/components/UserInteractionChecker/index.tsx
+++ src/client/components/UserInteractionChecker/index.tsx
@@ -1,6 +1,6 @@
 import { useAPI } from '@client/common/hooks/useAPI';
 import { useSession } from 'next-auth/react';
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 interface UserInteractionCheckerProps {
     children: React.ReactNode;
@@ -9,24 +9,51 @@ interface UserInteractionCheckerProps {
 const UserInteractionChecker: React.FunctionComponent<UserInteractionCheckerProps> = ({children}) => {
     const { data } = useSession()
     const request = useAPI('/api/user/interaction/interacting')
+    const once = useRef<boolean>(false)
 
     useEffect(() => {
         if(data?.user) {
-            const handle = () => {
-                const interacting = document.visibilityState === 'visible'
+            
+            // const handle = () => {
+            //     const interacting = document.visibilityState === 'visible'
 
+            //     request.call({
+            //         data: {
+            //             interacting
+            //         }
+            //     })
+            // }
+
+            // handle()
+
+            // document.addEventListener("visibilitychange", handle);
+
+            // return () => document.removeEventListener("visibilitychange", handle);
+
+            const handle = (interacting: boolean) => () => {
                 request.call({
                     data: {
                         interacting
                     }
                 })
             }
+
+            const handleBlur = handle(false)
+
+            const handleFocus = handle(true)
+
+            if(once.current === false) {
+                once.current = true
+                handleFocus()
+            }
             
-            handle()
+            window.addEventListener('blur', handleBlur, false)
+            window.addEventListener('focus', handleFocus, false)
 
-            document.addEventListener("visibilitychange", handle);
-
-            return () => document.removeEventListener("visibilitychange", handle);
+            return () => {
+                window.removeEventListener('blur', handleBlur, false)
+                window.removeEventListener('focus', handleFocus, false)
+            }
         }
     }, [data?.user])
 
